5c75218084e5d7f2e82446fc7aec2281bc2bef9c
Jing Liu <84009338+Jing-L97@users.noreply.github.com>
2026-03-12 17:21:28 +0100

    change bash

--- experiments/oberon/train/train_ppo.sh
+++ experiments/oberon/train/train_ppo.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=80G
 #SBATCH --cpus-per-task=8
 #SBATCH --time=24:00:00
-#SBATCH --output=/scratch2/jliu/Feedback/logs/ppo/new_%A_%a.log
+#SBATCH --output=/scratch2/jliu/Feedback/logs/ppo/check_%A_%a.log
 #SBATCH --array=0-4
 
 # Script and config paths
